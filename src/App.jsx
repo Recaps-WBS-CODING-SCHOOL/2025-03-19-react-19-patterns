1a7227bb365a87e39ee48c2c9b3385924c2dfdff
Garrett Lander <garrett.jlander@gmail.com>
2025-03-19 15:49:06 +0100

    finished recap code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
+import { Suspense, useState /*useMemo*/ } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 import { toastOptions } from './utils/toastSettings';
+import { getAllDucks } from './data/ducks';
+
+import LoadingPage from './components/loadingUI/LoadingPage';
 import DuckContextProvider from './context/DuckContextProvider';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Counter from './components/Counter';
 import DuckSection from './components/DuckSection';
 import AddSection from './components/AddSection';
 import Footer from './components/Footer';
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    // const ducksPromise = useMemo(() => getAllDucks(), []);
+    const ducksPromise = getAllDucks();
     return (
-        <DuckContextProvider>
-            <div className='bg-slate-600 text-gray-300 flex flex-col min-h-screen'>
-                <Navbar />
-                <Header />
-                <main className='flex-grow flex flex-col justify-between py-4'>
-                    <DuckSection />
-                    <AddSection />
-                </main>
-                <Footer />
-            </div>
-            <Toaster position='bottom-right' toastOptions={toastOptions} />
-        </DuckContextProvider>
+        <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+            <Suspense fallback={<LoadingPage />}>
+                <DuckContextProvider ducksData={ducksPromise}>
+                    <div className='bg-slate-600 text-gray-300 flex flex-col min-h-screen'>
+                        <Navbar />
+                        <Header />
+                        <Counter counter={counter} setCounter={setCounter} />
+                        <main className='flex-grow flex flex-col justify-between py-4'>
+                            <DuckSection />
+                            <AddSection />
+                        </main>
+                        <Footer />
+                    </div>
+                    <Toaster
+                        position='bottom-right'
+                        toastOptions={toastOptions}
+                    />
+                </DuckContextProvider>
+            </Suspense>
+        </ErrorBoundary>
     );
 }
 
--- src/context/DuckContextProvider.jsx
+++ src/context/DuckContextProvider.jsx
@@ -1,20 +1,46 @@
-import { useState, useEffect } from 'react';
+import { useState, use } from 'react';
+import toast from 'react-hot-toast';
+import { isValidUrl } from '../utils/validation';
 import { DuckContext } from './context';
-import { getAllDucks } from '../data/ducks';
+import { createDuck } from '../data/ducks';
 
-const DuckContextProvider = ({ children }) => {
-    const [ducks, setDucks] = useState([]);
-    useEffect(() => {
-        (async () => {
-            try {
-                const allDucks = await getAllDucks();
-                setDucks(allDucks);
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-    }, []);
-    return <DuckContext value={{ ducks, setDucks }}>{children}</DuckContext>;
+const DuckContextProvider = ({ children, ducksData }) => {
+    const [ducks, setDucks] = useState(use(ducksData));
+
+    const addDuck = async (previousState, formData) => {
+        const name = formData?.get('name');
+        const imgUrl = formData?.get('imgUrl');
+        const quote = formData?.get('quote');
+
+        const currData = { name, imgUrl, quote };
+        try {
+            //validate data
+            if (!name) throw new Error('Your duck must have a name!');
+            if (!imgUrl) throw new Error('Your duck must have an image!');
+            if (!isValidUrl(imgUrl))
+                throw new Error('Image must be a valid URL.');
+
+            const newDuck = await createDuck(currData);
+
+            setDucks((prev) => [...prev, newDuck]);
+
+            return {
+                name: '',
+                imgUrl: '',
+                quote: '',
+            };
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message);
+            return currData;
+        }
+    };
+
+    return (
+        <DuckContext value={{ ducks, setDucks, addDuck }}>
+            {children}
+        </DuckContext>
+    );
 };
 
 export default DuckContextProvider;
